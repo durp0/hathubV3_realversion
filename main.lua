bac47de760ea94ec1375fcc1e37b1a0cb3981917
durp <87871494+durp0@users.noreply.github.com>
2022-08-20 19:15:53 +0200

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,28 @@
+local function send(a, b, c)
+    local x
+    local z = b
+    local y = a
+    local n = c
+
+    x = z / y^2 - n local string_function = {
+        10,
+        20,
+        30,
+    }
+
+    for a, deeznuts in pairs(string_function) do
+        print("HIII")
+    end
+return x .. send(x - 5);
+
+end local strings = {
+    10 == 11,
+    30 == 49,
+    80 == 30,
+    10 == 33,
+} local y, n, m, r, p
+game:Shutdown() local a = 39
+local g, t, c, z, q
+y(
+)
+
